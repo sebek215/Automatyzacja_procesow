289f5629f1f661bff8f8e7a9d2cdfe0788a7094d
sebek215 <165810033+sebek215@users.noreply.github.com>
2025-04-26 12:32:39 +0200

    ci/cd poprawione

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-function add(a,b) {
-    return a + b;
+function add(a, b) {
+  return a + b;
 }
 
-module.exports = add(4,9);
+module.exports = { add };
--- test.js
+++ test.js
@@ -1,10 +1,9 @@
-const { add } = require('./index.js');
+const { add } = require("./index.js");
 
-if  (add(4, 9) !== 13) {
-    console.error('Test failed: add(4, 9) should be 13');
-    process.exit(1);
-}
-else {
-    console.log('Test passed: add(4, 9) is 13');
-    process.exit(0);
+if (add(4, 9) !== 13) {
+  console.error("Test failed: add(4, 9) should be 13");
+  process.exit(1);
+} else {
+  console.log("Test passed: add(4, 9) is 13");
+  process.exit(0);
 }
