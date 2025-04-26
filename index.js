a21246ead99212c357c19933653fb530f82d164d
sebek215 <165810033+sebek215@users.noreply.github.com>
2025-04-26 12:17:24 +0200

    second push

---
+++ index.js
@@ -0,0 +1,5 @@
+function add(a,b) {
+    return a + b;
+}
+
+module.exports = add(4,9);
---
+++ test.js
@@ -0,0 +1,10 @@
+const { add } = require('./index.js');
+
+if  (add(4, 9) !== 13) {
+    console.error('Test failed: add(4, 9) should be 13');
+    process.exit(1);
+}
+else {
+    console.log('Test passed: add(4, 9) is 13');
+    process.exit(0);
+}
